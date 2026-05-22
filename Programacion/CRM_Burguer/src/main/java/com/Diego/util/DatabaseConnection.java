3e03382faf3d4a0ae871e29a789ff25309c4be06
diegoo16 <dgargoles.06@gmail.com>
2026-05-22 10:09:55 +0200

    areglando cosas de java

--- Programacion/CRM_Burguer/src/main/java/com/Diego/util/DatabaseConnection.java
+++ Programacion/CRM_Burguer/src/main/java/com/Diego/util/DatabaseConnection.java
@@ -7,13 +7,22 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/hamburgueseria_crm";
+            "jdbc:mysql://localhost:3306/hamburgueseria_crm?serverTimezone=UTC";
 
-    private static final String USER = "root";
-    private static final String PASSWORD = "Admin1234";
+    private static final String USER = "root";   // o root
+    private static final String PASSWORD = "Admin1234";      // tu contraseña
 
-    public static Connection getConnection()
-            throws SQLException {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded correctly");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR: MySQL driver not found");
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
