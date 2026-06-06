399bf7dd96e8e01a83de2b8bcb5f678225fd75a1
diegoo16 <dgargoles.06@gmail.com>
2026-06-06 13:47:41 +0200

    mas arreglos dentro de model

--- Programacion/CRM_Burguer/src/main/java/com/Diego/model/OrderDetail.java
+++ Programacion/CRM_Burguer/src/main/java/com/Diego/model/OrderDetail.java
@@ -1,35 +1,38 @@
 package com.Diego.model;
 
-public class OrderDetail {
-    private int id;
+public class OrderDetail extends BaseEntity {
+
     private int orderId;
     private int productId;
     private int quantity;
     private double subtotal;
 
-    public OrderDetail(){}
-
-    public OrderDetail(int id,int orderId,int productId,
-                       int quantity,double subtotal){
-        this.id=id;
-        this.orderId=orderId;
-        this.productId=productId;
-        this.quantity=quantity;
-        this.subtotal=subtotal;
+    public OrderDetail() {
+        super();
     }
 
-    public int getId(){ return id; }
-    public void setId(int id){ this.id=id; }
+    public OrderDetail(int id, int orderId, int productId, int quantity, double subtotal) {
+        super(id);
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.subtotal = subtotal;
+    }
 
-    public int getOrderId(){ return orderId; }
-    public void setOrderId(int orderId){ this.orderId=orderId; }
+    public int getOrderId() { return orderId; }
+    public void setOrderId(int orderId) { this.orderId = orderId; }
 
-    public int getProductId(){ return productId; }
-    public void setProductId(int productId){ this.productId=productId; }
+    public int getProductId() { return productId; }
+    public void setProductId(int productId) { this.productId = productId; }
 
-    public int getQuantity(){ return quantity; }
-    public void setQuantity(int quantity){ this.quantity=quantity; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
 
-    public double getSubtotal(){ return subtotal; }
-    public void setSubtotal(double subtotal){ this.subtotal=subtotal; }
+    public double getSubtotal() { return subtotal; }
+    public void setSubtotal(double subtotal) { this.subtotal = subtotal; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Pedido: " + orderId + " | Producto: " + productId + " | Cant: " + quantity;
+    }
 }
